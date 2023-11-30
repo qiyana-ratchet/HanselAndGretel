756fdc6db48b8fde7d890c3614cc41df9307580a
qiyana-ratchet <wereter31@naver.com>
2023-11-30 17:52:06 +0900

    Witch tracking w/ manhatten distance

---
+++ src/main/java/org/example/Witch.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.awt.*;
+
+public class Witch {
+    private int x, y;  // Witch's position on the map
+
+    public Witch(int initialX, int initialY) {
+        x = initialX;
+        y = initialY;
+    }
+
+    public void trackPlayer(Player player) {
+        // Implement AI logic to track the player's position
+        // You can use Manhattan distance or any suitable algorithm
+        // Update the witch's position accordingly
+    }
+
+    // Implement methods to draw and control the witch character
+}
